5dfc982a853082418dd253e4ddef2e0a8a2d0d0a
OscarRubioSevilla <rubio.sevilla.oscar@gmail.com>
2023-05-15 16:45:50 -0600

    Updated: Styled initial button

--- src/components/EPG/Programmation.tsx
+++ src/components/EPG/Programmation.tsx
@@ -5,8 +5,8 @@ export function Programmation() {
   const [openModal, setOpenModal] = useState(false)
 
   return (
-    <div className="relative">
-      <button onClick={() => setOpenModal(true)}>Abrir Modal</button>
+    <div className="relative h-screen w-screen flex justify-center items-center">
+      <button onClick={() => setOpenModal(true)}>MOSTRAR EPG</button>
       {
         openModal ? <ChannelModal handleClose={() => setOpenModal(false)} /> : ''
       }
